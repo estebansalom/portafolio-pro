7f2c6c88b827f42f9f094de45080af19352c6483
estebansalom <esalomc@ucenfotec.ac.cr>
2024-12-06 17:08:51 -0600

    added more amplify code (attempting to deploy to another project)

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+    {
+      find: './runtimeConfig',
+      replacement: './runtimeConfig.browser',
+    },
+  ]
+},
+//Add build if you are going to use a Git-based (Github or CodeCommit) deployement
+build: {
+  outDir: "build",
+}
 })
